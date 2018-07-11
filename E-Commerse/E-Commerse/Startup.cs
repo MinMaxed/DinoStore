9e70d333d5d303f66e9c64f93555485d5b040102
Max <gusstorm132@gmail.com>
2018-07-11 12:22:40 -0700

    pushing merged branches

--- E-Commerse/E-Commerse/Startup.cs
+++ E-Commerse/E-Commerse/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECommerse.Data;
+using ECommerse.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
