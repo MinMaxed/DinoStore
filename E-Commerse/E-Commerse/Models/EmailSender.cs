8e32adba3d9ba279bad9f638b02e15591f7dd4a0
Benjamin Taylor <staylor.ben@gmail.com>
2018-07-23 14:53:51 -0700

    Added basic framework for sending emails.

---
+++ E-Commerse/E-Commerse/Models/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace ECommerse.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        IConfiguration Configuration { get; }
+        public EmailSender(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(Configuration["Keys:SendGrid"]);
+            var msg = new SendGridMessage();
+
+            msg.SetFrom("admin@dinostore.com", "DinoStore Admin");
+
+            msg.AddTo(email);
+            msg.SetSubject(subject);
+            msg.AddContent(MimeType.Html, htmlMessage);
+
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
