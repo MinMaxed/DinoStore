f92f48f046ffcf8079cfcf6cad288a4cf0c047d1
Benjamin Taylor <staylor.ben@gmail.com>
2018-07-30 10:20:01 -0700

    added fake cards into a drop-down list

--- E-Commerse/E-Commerse/Controllers/CheckoutController.cs
+++ E-Commerse/E-Commerse/Controllers/CheckoutController.cs
@@ -7,6 +7,7 @@ using ECommerse.Models;
 using ECommerse.Models.Interfaces;
 using ECommerse.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerse.Controllers
@@ -16,12 +17,15 @@ namespace ECommerse.Controllers
         private UserManager<ApplicationUser> _userManager;
         private IBasket _context;
         private IInventory _invContext;
+        private IEmailSender _emailSender;
 
-        public CheckoutController(IBasket context, UserManager<ApplicationUser> userManager, IInventory invContext)
+        public CheckoutController(IBasket context, UserManager<ApplicationUser> userManager,
+            IInventory invContext, IEmailSender emailSender)
         {
             _context = context;
             _userManager = userManager;
             _invContext = invContext;
+            _emailSender = emailSender;
         }
 
 
@@ -96,8 +100,12 @@ namespace ECommerse.Controllers
             
             ovm.Products = productList;
             ovm.UserOrder.Total = total;
+            ovm.OrderList = orderList;
 
             _invContext.UpdateOrder(ovm.UserOrder);
+
+            string htmlMessage = EmailGenerator.OrderConfirmationEmail(ovm, User.Claims.First(c => c.Type == "FullName").Value);
+            _emailSender.SendEmailAsync(ovm.UserOrder.UserEmail, "Your DinoStore Receipt", htmlMessage);
 
             return View(ovm);
         }
