92a04d8bf32d88b814a156ddea2f7428d4585ae9
Max <gusstorm132@gmail.com>
2018-07-29 20:40:52 -0700

    OrderListView

---
+++ E-Commerse/E-Commerse/Migrations/20180727193930_appsprint2deployagain2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerse.Migrations
+{
+    public partial class appsprint2deployagain2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
